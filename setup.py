488127376b42663a7a49f493b2a7fd9bb98e7e33
Stuart Berg <bergs@janelia.hhmi.org>
2023-09-12 11:45:20 -0400

    Added setup.py and versioneer integration

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""
+flyem-snapshot/setup.py
+"""
+from setuptools import find_packages, setup
+import versioneer
+
+# For now, requirements are only specified in the conda recipe, not here.
+
+setup(
+    name='flyem-snapshot',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Generate connectome exports, neuprint databases and reports from flat files and DVID checkpoints.',
+    url='https://github.com/janelia-flyem/flyem-snapshot',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'flyem-snapshot = flyem_snapshot.flyem_snapshot:main'
+        ]
+    }
+)
