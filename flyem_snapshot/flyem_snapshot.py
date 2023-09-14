d9e4d403265f40f9360b76dbc147bc291ae40903
Stuart Berg <bergs@janelia.hhmi.org>
2023-09-14 00:49:10 -0400

    fix config field

--- flyem_snapshot/flyem_snapshot.py
+++ flyem_snapshot/flyem_snapshot.py
@@ -133,7 +133,7 @@ def export_all(cfg, config_dir):
 
         # Produce outputs
         export_neuprint(cfg['outputs']['neuprint'], point_df, partner_df, ann, body_sizes)
-        export_reports(cfg['outputs']['reports'], point_df, partner_df, ann, snapshot_tag)
+        export_reports(cfg['outputs']['connectivity-reports'], point_df, partner_df, ann, snapshot_tag)
         export_flat_connectome(cfg['outputs']['flat-connectome'], point_df, partner_df, ann, snapshot_tag, min_conf)
 
 
