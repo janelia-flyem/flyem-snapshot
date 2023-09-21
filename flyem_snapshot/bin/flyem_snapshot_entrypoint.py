87192bdc5b891df0f9f5fe0942ed63a0a10bc38a
Stuart Berg <bergs@janelia.hhmi.org>
2023-09-21 14:30:27 -0400

    Renamed main file

--- flyem_snapshot/bin/flyem_snapshot_entrypoint.py
+++ flyem_snapshot/bin/flyem_snapshot_entrypoint.py
@@ -8,6 +8,7 @@ import argparse
 def main():
     DEBUG = False
     if DEBUG and len(sys.argv) == 1:
+        # This is simpler than messing around with VSCode debugging configurations.
         import os
         sys.stderr.write("DEBUGGING WITH ARTIFICAL ARGS\n")
         p = os.path.split(__file__)[0] + '/../../neuprint-small-test.yaml'
@@ -27,8 +28,8 @@ def main():
     if sum((bool(args.config), bool(args.dump_default_yaml), bool(args.dump_verbose_yaml))) > 1:
         sys.exit("You can't provide more than one of these options: -c, -y, -v")
 
-    import flyem_snapshot.flyem_snapshot
-    flyem_snapshot.flyem_snapshot.main(args)
+    import flyem_snapshot.main
+    flyem_snapshot.main.main(args)
 
 
 if __name__ == "__main__":
