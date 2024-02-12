janelia-flyem/flyem-snapshot

commit b18a68abca7195f4058c580866259bf2a41fbba4

Tons of fixes related to elements

flyem_snapshot/outputs/neuprint/synapse.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import logging
 import warnings
 from functools import partial
 
@@ -9,6 +10,8 @@ from neuclease.util import timed, compute_parallel, snakecase_to_camelcase
 from .util import append_neo4j_type_suffixes
 
 from .element import _export_element_group_csv
+
+logger = logging.getLogger(__name__)
 
 
 @PrefixFilter.with_context("Synapse")
@@ -40,13 +43,19 @@ def export_neuprint_synapses(cfg, point_df, tbar_nt):
     point_df[':Label'] = f'Synapse;{dataset}_Synapse;Element;{dataset}_Element'
     point_df['kind'] = point_df['kind'].cat.rename_categories({'PreSyn': 'pre', 'PostSyn': 'post'})
 
+    # All non-ROI columns from the input table are exported as :Element properties.
+    roicols = (*cfg['roi-set-names'], *(f'{c}_label' for c in cfg['roi-set-names']))
+    prop_cols = set(point_df.columns) - set(roicols) - {*'xyz', 'point_id'}
+
     # Note that :Synapses are :Elements and must be referenced that way in :CloseTo relationships.
     # Therefore, we use an ID space named "Element-ID", which is shared by Element nodes.
     point_df = point_df.rename(columns={
-        'point_id': ':ID(Element-ID)',
-        'kind': 'type:string',
-        'conf': 'confidence:float',
+        'point_id': ':ID(Element-ID)'
     })
+
+    logger.info(f"Non-ROI Synapse properties: {prop_cols}")
+    logger.info(f"ROI Synapse properties from the following roi-sets: {cfg['roi-set-names']}")
+    point_df = append_neo4j_type_suffixes(point_df, exclude=(*'xyz', *roicols))
 
     _export_fn = partial(
         _export_element_group_csv,

flyem_snapshot/bin/flyem_snapshot_entrypoint.py
@@ -17,7 +17,8 @@ def main():
         # than messing around with VSCode debugging configurations.
         import os
         sys.stderr.write("DEBUGGING WITH ARTIFICAL ARGS\n")
-        p = os.path.split(__file__)[0] + '/../../ol-small-test.yaml'
+        os.chdir('/Users/bergs/workspace/snapshot-configs/test-configs')
+        p = 'small-ol-test/ol-small-test.yaml'
         sys.argv.extend(['-c', p])
 
     parser = argparse.ArgumentParser(

flyem_snapshot/inputs/annotations.py
@@ -117,13 +117,19 @@ def load_annotations(cfg, dvidseg, snapshot_tag):
     vc = vc[vc.index != ""]
     vc.to_csv(f'tables/status-counts-{snapshot_tag}.csv', index=True, header=True)
 
-    title = f'body status counts ({snapshot_tag})'
-    p = vc.hvplot.barh(flip_yaxis=True, title=title)
-    export_bokeh(
-        hv.render(p),
-        f"body-status-counts-{snapshot_tag}.html",
-        title
-    )
+    try:
+        title = f'body status counts ({snapshot_tag})'
+        p = vc.hvplot.barh(flip_yaxis=True, title=title)
+        export_bokeh(
+            hv.render(p),
+            f"body-status-counts-{snapshot_tag}.html",
+            title
+        )
+    except RuntimeError as ex:
+        if 'geckodriver' in str(ex):
+            logger.warning(f"Not exporting body-status-counts graph: {str(ex)}")
+        else:
+            raise
 
     if cfg['point-annotations'] and not dvidseg:
         raise RuntimeError("Can't read point-annotations without a dvid segmentation.")

flyem_snapshot/inputs/dvidseg.py
@@ -4,9 +4,7 @@ import pyarrow.feather as feather
 from neuclease.util import dump_json
 from neuclease.dvid.repo import fetch_branch_nodes
 from neuclease.dvid.labelmap import fetch_complete_mappings, fetch_mutations
-from neuclease.dvid.labelmap.pointlabeler import PointLabeler
-
-DvidSeg = namedtuple('DvidSeg', 'server uuid instance')
+from neuclease.dvid.labelmap.pointlabeler import PointLabeler, DvidSeg
 
 DvidSegSchema = {
     "description": "dvid segmentation (labelmap) location",
@@ -55,7 +53,7 @@ def load_dvidseg(cfg, snapshot_tag):
 
     # Look for the last mutation, searching backwards in the DAG until a non-empty UUID is found.
     last_mutation = {}
-    branch_nodes = fetch_branch_nodes(*dvidseg)
+    branch_nodes = fetch_branch_nodes(dvidseg.server, dvidseg.uuid)
     for uuid in branch_nodes[::-1]:
         muts = fetch_mutations(dvidseg.server, uuid, dvidseg.instance, dag_filter='leaf-only')
         if len(muts):

flyem_snapshot/inputs/elements.py
@@ -26,11 +26,6 @@ ElementTableSchema = {
             "type": "string",
             "default": ""
         },
-        "neuprint-label": {
-            "description": "A specific node label, if any, to use when exporting these elements to neuprint, e.g. ':Mito'\n",
-            "type": "string",
-            "default": ""
-        },
         "type": {
             "description":
                 "Optional. Adds a column named 'type' to the table, with the given value in all rows.\n"

flyem_snapshot/inputs/rois.py
@@ -81,7 +81,7 @@ RoisSchema = {
         "dvid": {
             "description":
                 "Which DVID server to use for reading ROIs.\n"
-                "If not specified here, the settings from the 'synapses.update-to' setting will be used.\n",
+                "If not specified here, the settings from the 'dvid-seg' config section will be used.\n",
             "default": {},
             "additionalProperties": False,
             "properties": {

flyem_snapshot/inputs/synapses.py
@@ -74,17 +74,6 @@ SnapshotSynapsesSchema = {
             "type": "string",
             # NO DEFAULT
         },
-        "update-to": {
-            "oneOf": [
-                LabelmapSchema,
-                {"type": "null"}
-            ],
-            "default": {},
-            "description":
-                "DVID labelmap instance to use for updating the synapse body IDs.\n"
-                "If not provided, the synapse point_df table must include a 'body' column,\n"
-                "which will be used without modifications.\n",
-        },
         "zone": {
             # TODO: Eliminate the 'zone' feature in favor of more flexibility in defining report ROIs.
             "description":

flyem_snapshot/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 import logging
 from collections.abc import Mapping
 
@@ -229,6 +230,9 @@ def load_inputs(cfg):
         element_tables[elm_name] = (elm_points, elm_distances)
         element_roisets[elm_name] = elm_roisets
 
+    with open('tables/element_tables.pkl', 'wb') as f:
+        pickle.dump(element_tables, f)
+
     #
     # Body sizes (for synaptic bodies only)
     #
@@ -250,8 +254,10 @@ def produce_outputs(cfg, last_mutation, ann, element_tables, point_df, partner_d
     min_conf = cfg['inputs']['synapses']['min-confidence']
 
     export_neurotransmitters(cfg['outputs']['neurotransmitters'], tbar_nt, body_nt, point_df)
+
     export_neuprint(cfg['outputs']['neuprint'], point_df, partner_df, element_tables, ann, body_sizes,
                     tbar_nt, body_nt, syn_roisets, element_roisets, last_mutation)
+
     export_flat_connectome(cfg['outputs']['flat-connectome'], point_df, partner_df, ann, snapshot_tag, min_conf)
     export_reports(cfg['outputs']['connectivity-reports'], point_df, partner_df, ann, snapshot_tag)
 
@@ -267,7 +273,7 @@ def determine_snapshot_tag(cfg, config_dir):
     FIXME: Ideally, we should also consult the annotation instance
             to see if it has even newer dates.
     """
-    dvidcfg = cfg['inputs']['dvid-seg']
+    dvidcfg = cfg['inputs'].get('dvid-seg', {"server": None})
     uuid = None
     if dvidcfg['server']:
         uuid, snapshot_tag = resolve_snapshot_tag(
@@ -324,7 +330,7 @@ def _finalize_config_and_output_dir(cfg, config_dir):
            we should probably move some of this logic into the relevant pipeline steps.
     """
     jobcfg = cfg['job-settings']
-    dvidcfg = cfg['dvid-seg']
+    dvidcfg = cfg['inputs']['dvid-seg'] = cfg['inputs'].get('dvid-seg', {"server": None})
     syncfg = cfg['inputs']['synapses']
     roicfg = cfg['inputs']['rois']
     neuprintcfg = cfg['outputs']['neuprint']

commit 309723274ed37bd24e07a8cd207232cfaa3d0586

neuprint: Fix synapse property names for 'type' and 'confidence', broken in b18a68abca7195f4058c580866259bf2a41fbba4

flyem_snapshot/outputs/neuprint/synapse.py
@@ -51,7 +51,9 @@ def export_neuprint_synapses(cfg, point_df, tbar_nt):
     # Note that :Synapses are :Elements and must be referenced that way in :CloseTo relationships.
     # Therefore, we use an ID space named "Element-ID", which is shared by Element nodes.
     point_df = point_df.rename(columns={
-        'point_id': ':ID(Element-ID)'
+        'point_id': ':ID(Element-ID)',
+        'kind': 'type:string',
+        'conf': 'confidence:float',
     })
 
     logger.info(f"Non-ROI Synapse properties: {prop_cols}")