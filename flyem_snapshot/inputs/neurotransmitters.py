janelia-flyem/flyem-snapshot

commit 0429f830dd55a7330ab91e45f5fec68cdfe3f21c

Refactor the caching logic for synapses and elements

flyem_snapshot/inputs/neurotransmitters.py
@@ -310,12 +310,12 @@ def _compute_body_neurotransmitters(tbar_nt, gt_df, ann, min_body_conf, min_body
             on='cell_type',
             suffixes=['', '_celltype']
         )
-        .drop(columns=['num_tbar_nt_predictions'])
         .rename(columns={
             'top_pred': 'predicted_nt',
             'top_pred_celltype': 'celltype_predicted_nt',
             'confidence': 'predicted_nt_confidence',
             'confidence_celltype': 'celltype_predicted_nt_confidence',
+            'num_tbar_nt_predictions': 'total_nt_predictions',
             'num_tbar_nt_predictions_celltype': 'celltype_total_nt_predictions',
         })
     )
@@ -415,15 +415,17 @@ def _calc_group_predictions(pred_df, ann, confusion_df, gt_df, groupcol):
         # Append the 'cell_type' column
         assert df.index.name == ann_types.index.name == 'body'
         df = df.merge(ann_types, 'outer', on='body')
-        df = df.reset_index()
-        assert 'body' in df.columns
+        assert df.index.name == ann_types.index.name == 'body'
     else:
+        # Make sure all cell_types from ann are listed in df
         ann_types = ann_types.drop_duplicates()
-        df = df.merge(ann_types, 'outer', on='cell_type')
+        df = df.merge(ann_types, 'outer', on='cell_type').set_index('cell_type')
 
-    assert 'cell_type' in df.columns
     df['num_tbar_nt_predictions'] = pred_df.groupby(groupcol)['pred1'].count()
-    df = df.sort_values(groupcol)
+    df['num_tbar_nt_predictions'].fillna(0, inplace=True)
+
+    assert df.index.name == groupcol
+    df = df.sort_index().reset_index()
 
     df = df.sort_index()
     # Without groundtruth, all we can provide are

flyem_snapshot/caches.py
@@ -1,0 +1,71 @@
+import os
+import logging
+import functools
+from abc import abstractmethod
+
+import pandas as pd
+import pyarrow.feather as feather
+
+logger = logging.getLogger(__name__)
+
+
+def cached(serializer, name, cache_dir='tables'):
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            key = serializer.get_cache_key(*args, **kwargs)
+            result_path = f'{cache_dir}/{key}'
+            if os.path.exists(result_path):
+                try:
+                    logger.info(f"Loading {name} from cache: {result_path}")
+                    return serializer.load_from_file(result_path)
+                except Exception as ex:  # pylint: disable=broad-exception-caught
+                    logger.error(f"Ignoring {name} cache due to error: {ex}")
+
+            result = f(*args, **kwargs)
+            logger.info(f"Storing {name} to cache: {result_path}")
+            serializer.save_to_file(result, result_path)
+            return result
+        return wrapper
+    decorator.serializer = serializer
+    return decorator
+
+
+class SerializerBase:
+
+    @abstractmethod
+    def get_cache_key(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def save_to_file(self, result, path):
+        raise NotImplementedError
+
+    @abstractmethod
+    def load_from_file(self, path):
+        raise NotImplementedError
+
+
+class DataFrameSerializer(SerializerBase):
+
+    def __init__(self, save_index=True):
+        self.save_index = save_index
+
+    @abstractmethod
+    def save_to_file(self, result, path):
+        if self.save_index:
+            assert not isinstance(result.index, pd.MultiIndex), \
+                "Saving DataFrames with MultiIndex is not supported"
+            feather.write_feather(result.reset_index(), path)
+        else:
+            feather.write_feather(result, path)
+
+    @abstractmethod
+    def load_from_file(self, path):
+        if self.save_index:
+            df = feather.read_feather(path)
+            df = df.set_index(df.columns[0])
+            return df
+        else:
+            return feather.read_feather(path)
+

flyem_snapshot/inputs/annotations.py
@@ -79,19 +79,28 @@ AnnotationsSchema = {
 
 
 @PrefixFilter.with_context('annotations')
-def load_annotations(cfg, dvidseg, snapshot_tag):
+def load_annotations(cfg, pointlabeler, snapshot_tag):
     """
     Load body annotations, either from a feather file or from DVID.
     If from a file, it MUST contain a 'body' column.
 
     If a list of 'point-annotations' instances are listed,
     they will be used to add (or overwrite) columns in the
+
+    Note:
+        We don't cache the annotation results because there's not really
+        a faster way to determine if the annotations in DVID have changed
+        that is much faster than just loading them from scratch anyway.
     """
     if cfg['body-annotations-table']:
         logger.info("Reading body annotations table from disk INSTEAD of reading from DVID.")
         ann = feather.read_feather(cfg['body-annotations-table']).set_index('body')
     else:
-        ann = fetch_body_annotations(dvidseg.server, dvidseg.uuid, dvidseg.instance + '_annotations')
+        ann = fetch_body_annotations(
+            pointlabeler.dvidseg.server,
+            pointlabeler.dvidseg.uuid,
+            pointlabeler.dvidseg.instance + '_annotations'
+        )
 
         # Feather seems to have a hard time if empty strings are in otherwise int columns.
         # Currently, it's legitimate to replace '' with None for all neuprint properties we have so far,
@@ -133,7 +142,7 @@ def load_annotations(cfg, dvidseg, snapshot_tag):
         else:
             raise
 
-    if cfg['point-annotations'] and not dvidseg:
+    if cfg['point-annotations'] and not pointlabeler:
         raise RuntimeError("Can't read point-annotations without a dvid segmentation.")
 
     # Anything mentioned in the point-annotations config
@@ -141,10 +150,15 @@ def load_annotations(cfg, dvidseg, snapshot_tag):
     for pa in cfg['point-annotations']:
         col = pa['column-name'] or pa['instance']
         with PrefixFilter.context(pa['instance']):
-            df = fetch_all_elements(dvidseg.server, dvidseg.uuid, pa['instance'], format='pandas')
+            df = fetch_all_elements(
+                pointlabeler.dvidseg.server,
+                pointlabeler.dvidseg.uuid,
+                pa['instance'],
+                format='pandas'
+            )
             df = df.sort_values([*'zyx'])
             df['body'] = fetch_labels_batched(
-                *dvidseg,
+                *pointlabeler.dvidseg,
                 df[[*'zyx']].values,
                 processes=cfg['processes']
             )

flyem_snapshot/inputs/dvidseg.py
@@ -1,9 +1,7 @@
-from collections import namedtuple
 import pyarrow.feather as feather
 
 from neuclease.util import dump_json
-from neuclease.dvid.repo import fetch_branch_nodes
-from neuclease.dvid.labelmap import fetch_complete_mappings, fetch_mutations
+from neuclease.dvid.labelmap import fetch_complete_mappings
 from neuclease.dvid.labelmap.pointlabeler import PointLabeler, DvidSeg
 
 DvidSegSchema = {
@@ -51,15 +49,6 @@ def load_dvidseg(cfg, snapshot_tag):
             f"tables/complete-nonsingleton-mapping-{snapshot_tag}.feather"
         )
 
-    # Look for the last mutation, searching backwards in the DAG until a non-empty UUID is found.
-    last_mutation = {}
-    branch_nodes = fetch_branch_nodes(dvidseg.server, dvidseg.uuid)
-    for uuid in branch_nodes[::-1]:
-        muts = fetch_mutations(dvidseg.server, uuid, dvidseg.instance, dag_filter='leaf-only')
-        if len(muts):
-            last_mutation = muts.iloc[-1].to_dict()
-            break
-
-    dump_json(last_mutation, 'tables/last-mutation.json')
     pointlabeler = PointLabeler(*dvidseg, mapping=mapping)
-    return dvidseg, last_mutation, pointlabeler
+    dump_json(pointlabeler.last_mutation, 'tables/last-mutation.json')
+    return pointlabeler

flyem_snapshot/inputs/rois.py
@@ -116,7 +116,24 @@ RoisSchema = {
 def load_point_rois(cfg, point_df, roiset_names):
     """
     For each named ROI set in the config,
-    add a column to point_df for the ROI of the PostSyn side.
+    add a column to point_df for the ROI of the PostSyn side,
+    and return the modified point_df table.
+
+    Also returns the dict-of-dict for roi names and IDs, structured like this:
+
+        {
+            roiset-name: {
+                roi-name: int,
+                roi-name: int,
+                ...
+            },
+            roiset-name: {
+                roi-name: int,
+                roi-name: int,
+                ...
+            },
+            ...
+        }
     """
     if point_df is None:
         return None, {}

flyem_snapshot/inputs/sizes.py
@@ -48,7 +48,7 @@ BodySizesSchema = {
 
 
 @PrefixFilter.with_context('body-sizes')
-def load_body_sizes(cfg, dvidseg, df, snapshot_tag):
+def load_body_sizes(cfg, pointlabeler, df, snapshot_tag):
     """
     Load/export the sizes of all bodies listed in df
     (in either the index or the 'body' column).
@@ -60,6 +60,7 @@ def load_body_sizes(cfg, dvidseg, df, snapshot_tag):
         logger.info("Body sizes will not be emitted due to load-sizes: false")
         return None
 
+    dvidseg = pointlabeler and pointlabeler.dvidseg
     cache_file = cfg['cache-file']
     cache_uuid = cfg['cache-uuid']
     if not cache_file and not dvidseg:

flyem_snapshot/inputs/synapses.py
@@ -3,6 +3,10 @@
 Import synapses from disk, filter them, and associate each point with a body ID.
 """
 import os
+import re
+import copy
+import glob
+import json
 import logging
 
 import numpy as np
@@ -11,6 +15,7 @@ import pyarrow.feather as feather
 from neuclease import PrefixFilter
 from neuclease.util import Timer, encode_coords_to_uint64, decode_coords_from_uint64
 
+from ..caches import cached, SerializerBase
 
 logger = logging.getLogger(__name__)
 
@@ -115,18 +120,50 @@ SnapshotSynapsesSchema = {
 }
 
 
-@PrefixFilter.with_context('synapses')
-def load_synapses(cfg, snapshot_tag, pointlabeler):
-    os.makedirs('tables', exist_ok=True)
+class SynapseSerializerBase(SerializerBase):
 
-    # Do the files already exist for this snapshot?
-    # If so, just load those and return.
-    if os.path.exists(f'tables/partner_df-{snapshot_tag}.feather'):
-        logger.info("Loading previously-written synapse files")
-        partner_df = feather.read_feather(f'tables/partner_df-{snapshot_tag}.feather')
-        point_df = feather.read_feather(f'tables/point_df-{snapshot_tag}.feather').set_index('point_id')
+    def save_to_file(self, result, path):
+        point_df, partner_df = result
+        os.makedirs(path, exist_ok=True)
+
+        # It's not really necessary to put the snapshot tag, etc. in the file names,
+        # since that stuff is already in the directory name.
+        # But it's convenient if we want to copy these files for other people to use.
+        name = os.path.split(path)[-1]
+        feather.write_feather(
+            point_df.reset_index(),
+            f'{path}/point_df-{name}.feather'
+        )
+        feather.write_feather(
+            partner_df,
+            f'{path}/partner_df-{name}.feather'
+        )
+
+    def load_from_file(self, path):
+        point_path = glob.glob(f'{path}/point_df-*.feather')[0]
+        partner_path = glob.glob(f'{path}/partner_df-*.feather')[0]
+        point_df = feather.read_feather(point_path).set_index('point_id')
+        partner_df = feather.read_feather(partner_path)
         return point_df, partner_df
 
+
+class RawSynapseSerializer(SynapseSerializerBase):
+
+    def get_cache_key(self, cfg, snapshot_tag, pointlabeler):
+        cfg = copy.deepcopy(cfg)
+        cfg['processes'] = 0
+        cfg_hash = abs(hash(json.dumps(cfg, sort_keys=True)))
+
+        if pointlabeler is None:
+            return f'synapses-{snapshot_tag}-cfg-{cfg_hash}'
+
+        mutid = pointlabeler.last_mutation["mutid"]
+        return f'{snapshot_tag}-seg-{mutid}-syn-{cfg_hash}'
+
+
+@PrefixFilter.with_context('synapses')
+@cached(RawSynapseSerializer(), 'labeled-synapses')
+def load_synapses(cfg, snapshot_tag, pointlabeler):
     point_df, partner_df = _load_raw_synapses(cfg)
 
     if pointlabeler:
@@ -143,23 +180,6 @@ def load_synapses(cfg, snapshot_tag, pointlabeler):
         partner_df = partner_df.merge(point_df['body'], 'left', left_on='pre_id', right_index=True)
         partner_df = partner_df.merge(point_df['body'], 'left', left_on='post_id', right_index=True, suffixes=['_pre', '_post'])
 
-    with Timer("Exporting filtered/updated synapse tables", logger):
-        export_synapse_cache(point_df, partner_df, snapshot_tag)
-
-    return point_df, partner_df
-
-
-def export_synapse_cache(point_df, partner_df, snapshot_tag):
-    # Overwrite the point_df we saved earlier now that we've updated the ROIs.
-    with Timer("Exporting filtered/updated synapse tables", logger):
-        feather.write_feather(
-            point_df.reset_index(),
-            f'tables/point_df-{snapshot_tag}.feather'
-        )
-        feather.write_feather(
-            partner_df,
-            f'tables/partner_df-{snapshot_tag}.feather'
-        )
     return point_df, partner_df
 
 

flyem_snapshot/main.py
@@ -1,18 +1,20 @@
 import os
 import sys
+import copy
+import json
 import pickle
 import logging
 from collections.abc import Mapping
 
 from confiddler import dump_default_config, load_config, dump_config
 from neuclease import configure_default_logging, PrefixFilter
-from neuclease.util import Timer, switch_cwd
+from neuclease.util import Timer, switch_cwd, dump_json
 from neuclease.dvid import set_default_dvid_session_timeout
 from neuclease.dvid.labelmap import resolve_snapshot_tag
 
 from .inputs.dvidseg import DvidSegSchema, load_dvidseg
 from .inputs.elements import ElementTablesSchema, load_elements
-from .inputs.synapses import SnapshotSynapsesSchema, load_synapses, export_synapse_cache
+from .inputs.synapses import SnapshotSynapsesSchema, load_synapses, RawSynapseSerializer
 from .inputs.annotations import AnnotationsSchema, load_annotations
 from .inputs.rois import RoisSchema, load_point_rois, merge_partner_rois
 from .inputs.sizes import BodySizesSchema, load_body_sizes
@@ -24,6 +26,7 @@ from .outputs.neuprint import NeuprintSchema, export_neuprint
 from .outputs.neuprint.meta import NeuprintMetaSchema
 from .outputs.reports import ReportsSchema, export_reports
 
+from .caches import cached, SerializerBase
 from .util import log_lsf_details
 
 logger = logging.getLogger(__name__)
@@ -175,24 +178,63 @@ def export_all(cfg, config_dir):
 
 def load_inputs(cfg):
     snapshot_tag = cfg['job-settings']['snapshot-tag']
+    pointlabeler = load_dvidseg(cfg['inputs']['dvid-seg'], snapshot_tag)
+    ann = load_annotations(cfg['inputs']['annotations'], pointlabeler, snapshot_tag)
+    point_df, partner_df, syn_roisets = load_synapses_and_rois(cfg, pointlabeler)
+    element_tables, element_roisets = load_elements_and_rois(cfg, pointlabeler)
+    body_sizes = load_body_sizes(cfg['inputs']['body-sizes'], pointlabeler, point_df, snapshot_tag)
+    tbar_nt, body_nt = load_neurotransmitters(cfg['inputs']['neurotransmitters'], point_df, partner_df, ann)
+    return (pointlabeler, ann, element_tables, point_df, partner_df,
+            syn_roisets, element_roisets, body_sizes, tbar_nt, body_nt)
 
-    #
-    # Segmentation parameters
-    #
-    dvidseg, last_mutation, pointlabeler = load_dvidseg(cfg['inputs']['dvid-seg'], snapshot_tag)
 
-    #
-    # Annotations
-    #
-    ann = load_annotations(
-        cfg['inputs']['annotations'],
-        dvidseg,
-        snapshot_tag
-    )
+def produce_outputs(cfg, pointlabeler, ann, element_tables, point_df, partner_df,
+                    syn_roisets, element_roisets, body_sizes, tbar_nt, body_nt):
 
-    #
-    # Synapses
-    #
+    snapshot_tag = cfg['job-settings']['snapshot-tag']
+    min_conf = cfg['inputs']['synapses']['min-confidence']
+
+    export_neurotransmitters(cfg['outputs']['neurotransmitters'], tbar_nt, body_nt, point_df)
+
+    export_neuprint(cfg['outputs']['neuprint'], point_df, partner_df, element_tables, ann, body_sizes,
+                    tbar_nt, body_nt, syn_roisets, element_roisets, pointlabeler)
+
+    export_flat_connectome(cfg['outputs']['flat-connectome'], point_df, partner_df, ann, snapshot_tag, min_conf)
+    export_reports(cfg['outputs']['connectivity-reports'], point_df, partner_df, ann, snapshot_tag)
+
+
+class SynapsesWithRoiSerializer(SerializerBase):
+
+    def get_cache_key(self, cfg, pointlabeler):
+        snapshot_tag = cfg['job-settings']['snapshot-tag']
+        cfg = copy.deepcopy(cfg)
+        cfg['inputs']['synapses']['processes'] = 0
+        cfg['inputs']['rois']['processes'] = 0
+        syn_hash = abs(hash(json.dumps(cfg['inputs']['synapses'], sort_keys=True)))
+        roi_hash = abs(hash(json.dumps(cfg['inputs']['rois'], sort_keys=True)))
+
+        if pointlabeler is None:
+            return f'{snapshot_tag}-syn-{syn_hash}-roi-{roi_hash}'
+
+        mutid = pointlabeler.last_mutation["mutid"]
+        return f'{snapshot_tag}-seg-{mutid}-syn-{syn_hash}-roi-{roi_hash}'
+
+    def save_to_file(self, result, path):
+        point_df, partner_df, syn_roisets = result
+        os.makedirs(path, exist_ok=True)
+        RawSynapseSerializer().save_to_file((point_df, partner_df), path)
+        dump_json(syn_roisets, f'{path}/roisets.json')
+
+    def load_from_file(self, path):
+        point_df, partner_df = RawSynapseSerializer().load_from_file(path)
+        with open(f'{path}/roisets.json', 'r') as f:
+            syn_roisets = json.load(f)
+        return point_df, partner_df, syn_roisets
+
+
+@cached(SynapsesWithRoiSerializer(), 'labeled-synapses-with-rois')
+def load_synapses_and_rois(cfg, pointlabeler):
+    snapshot_tag = cfg['job-settings']['snapshot-tag']
     point_df, partner_df = load_synapses(
         cfg['inputs']['synapses'],
         snapshot_tag,
@@ -213,11 +255,39 @@ def load_inputs(cfg):
             cfg['inputs']['synapses']['roi-set-names']
         )
 
-    export_synapse_cache(point_df, partner_df, snapshot_tag)
+    return point_df, partner_df, syn_roisets
 
-    #
-    # Elements
-    #
+
+class ElementsWithRoiSerializer(SerializerBase):
+
+    def get_cache_key(self, cfg, pointlabeler):
+        snapshot_tag = cfg['job-settings']['snapshot-tag']
+        cfg = copy.deepcopy(cfg)
+        cfg['inputs']['elements']['processes'] = 0
+        cfg['inputs']['rois']['processes'] = 0
+        elm_hash = abs(hash(json.dumps(cfg['inputs']['elements'], sort_keys=True)))
+        roi_hash = abs(hash(json.dumps(cfg['inputs']['rois'], sort_keys=True)))
+
+        if pointlabeler is None:
+            return f'{snapshot_tag}-elm-{elm_hash}-roi-{roi_hash}'
+
+        mutid = pointlabeler.last_mutation["mutid"]
+        return f'{snapshot_tag}-seg-{mutid}-elm-{elm_hash}-roi-{roi_hash}'
+
+    def save_to_file(self, result, path):
+        os.makedirs(path, exist_ok=True)
+        element_tables, element_roisets = result
+        with open(f'{path}/element-cache.pkl', 'wb') as f:
+            pickle.dump((element_tables, element_roisets), f)
+
+    def load_from_file(self, path):
+        with open(f'{path}/element-cache.pkl', 'rb') as f:
+            element_tables, element_roisets = pickle.load(f)
+        return element_tables, element_roisets
+
+
+@cached(ElementsWithRoiSerializer(), 'labeled-synapses-with-rois')
+def load_elements_and_rois(cfg, pointlabeler):
     element_tables = load_elements(cfg['inputs']['elements'], pointlabeler)
     element_roisets = {}
     for elm_name in list(element_tables.keys()):
@@ -233,36 +303,7 @@ def load_inputs(cfg):
         element_tables[elm_name] = (elm_points, elm_distances)
         element_roisets[elm_name] = elm_roisets
 
-    with open('tables/element_tables.pkl', 'wb') as f:
-        pickle.dump(element_tables, f)
-
-    #
-    # Body sizes (for synaptic bodies only)
-    #
-    body_sizes = load_body_sizes(cfg['inputs']['body-sizes'], dvidseg, point_df, snapshot_tag)
-
-    #
-    # Neurotransmitters
-    #
-    tbar_nt, body_nt = load_neurotransmitters(cfg['inputs']['neurotransmitters'], point_df, partner_df, ann)
-
-    return (last_mutation, ann, element_tables, point_df, partner_df,
-            syn_roisets, element_roisets, body_sizes, tbar_nt, body_nt)
-
-
-def produce_outputs(cfg, last_mutation, ann, element_tables, point_df, partner_df,
-                    syn_roisets, element_roisets, body_sizes, tbar_nt, body_nt):
-
-    snapshot_tag = cfg['job-settings']['snapshot-tag']
-    min_conf = cfg['inputs']['synapses']['min-confidence']
-
-    export_neurotransmitters(cfg['outputs']['neurotransmitters'], tbar_nt, body_nt, point_df)
-
-    export_neuprint(cfg['outputs']['neuprint'], point_df, partner_df, element_tables, ann, body_sizes,
-                    tbar_nt, body_nt, syn_roisets, element_roisets, last_mutation)
-
-    export_flat_connectome(cfg['outputs']['flat-connectome'], point_df, partner_df, ann, snapshot_tag, min_conf)
-    export_reports(cfg['outputs']['connectivity-reports'], point_df, partner_df, ann, snapshot_tag)
+    return element_tables, element_roisets
 
 
 def determine_snapshot_tag(cfg, config_dir):
@@ -275,6 +316,12 @@ def determine_snapshot_tag(cfg, config_dir):
 
     FIXME: Ideally, we should also consult the annotation instance
             to see if it has even newer dates.
+
+    Example default snapshot tags:
+
+        - 2023-11-03-abc123
+        - 2023-11-04-def456-unlocked
+
     """
     dvidcfg = cfg['inputs'].get('dvid-seg', {"server": None})
     uuid = None

commit b599c711413c7790b68ddd6b29b2ac99cea9470b

neurotransmitters: bug fix: Remove mistaken reset_index() call that caused confusion scores to be incorrect. I think the bug was introduced in 0429f830dd55a7330ab91e45f5fec68cdfe3f21c, probably due to sloppy git rebasing

flyem_snapshot/inputs/neurotransmitters.py
@@ -497,7 +497,7 @@ def _calc_group_predictions(pred_df, ann, confusion_df, gt_df, groupcol):
     df['num_tbar_nt_predictions'].fillna(0, inplace=True)
 
     assert df.index.name == groupcol
-    df = df.sort_index().reset_index()
+    df = df.sort_index()
 
     # Without groundtruth, all we can provide are
     # the aggregated values -- no confidences
@@ -524,6 +524,8 @@ def _calc_group_predictions(pred_df, ann, confusion_df, gt_df, groupcol):
     group_pred_and_syn_pred = pred_df.loc[valid_rows, ['group_pred', 'pred1']]
     group_pred_and_syn_pred = pd.MultiIndex.from_frame(group_pred_and_syn_pred)
     pred_df.loc[valid_rows, 'confusion_score'] = confusion_df.stack().loc[group_pred_and_syn_pred].values
+
+    assert df.index.name == groupcol
     df['mean_confusion'] = pred_df.groupby(groupcol)['confusion_score'].mean().fillna(0.0)
 
     # If there were no tbar NT predictions provided at all