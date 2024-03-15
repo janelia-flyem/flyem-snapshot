janelia-flyem/flyem-snapshot

commit a51287d015393ad4e58d2c4b265fdc991459f63b

neurotransmitters: Replace np.nan with None to ensure that the body_df has the same checksum whether or not it came from cache.

flyem_snapshot/util.py
@@ -9,6 +9,7 @@ from collections.abc import Mapping, Sequence
 
 import numpy as np
 import pandas as pd
+import pyarrow.feather as feather
 from bokeh.plotting import output_file, save as bokeh_save
 from bokeh.io import export_png
 
@@ -18,6 +19,48 @@ def rm_f(path):
         os.unlink(path)
     except FileNotFoundError:
         pass
+
+
+def cache_dataframe(df, path):
+    """
+    A wrapper around feather.write_feather with an extra check to ensure
+    that NaN data will not change after a round-trip of write/read.
+    Raises an error if the dataframe doesn't conform.
+    """
+    # Standardize on None as the null value (instead of NaN or "").
+    # https://stackoverflow.com/questions/46283312/how-to-proceed-with-none-value-in-pandas-fillna
+    FAKENULL = '__cache_dataframe_nullval__'
+    bad_columns = []
+    for col, dtype in df.dtypes.items():
+        if dtype != object:
+            continue
+
+        if (df[col].replace([np.nan], [FAKENULL]) != df[col].replace([None], FAKENULL)).any():
+            bad_columns.append(col)
+    if bad_columns:
+        msg = (
+            "DataFrame cannot be cached because it contains column(s) of 'object' dtype "
+            "that contain np.nan. write_feather() will convert them to None, giving them "
+            "a different checksum.  Convert them to None yourself using Series.replace([np.nan], [None])) "
+            "before returning the dataframe, so it can be cached.\n"
+            f"The nan-containing columns are: [{bad_columns}]"
+        )
+        raise RuntimeError(msg)
+    feather.write_feather(df, path)
+
+
+def replace_object_nan_with_none(df):
+    """
+    For columns in the given DataFrame with dtype 'object',
+    ensure that None is used as the null value rather than np.nan.
+    This ensures perfect round-trip feather serialization, for instance.
+
+    Works in-place.
+    """
+    for col, dtype in df.dtypes.items():
+        if dtype != object:
+            continue
+        df[col].replace([np.nan, None], inplace=True)
 
 
 def dataframe_checksum(df):

flyem_snapshot/inputs/neurotransmitters.py
@@ -10,7 +10,7 @@ import pyarrow.feather as feather
 from neuclease import PrefixFilter
 from neuclease.util import timed, encode_coords_to_uint64
 
-from ..util import restrict_synapses_to_roi, checksum
+from ..util import restrict_synapses_to_roi, checksum, cache_dataframe, replace_object_nan_with_none
 from ..caches import cached, SerializerBase
 
 logger = logging.getLogger(__name__)
@@ -160,10 +160,10 @@ class NeurotransmitterSerializer(SerializerBase):
         assert tbar_df.index.name == 'point_id'
         assert body_df.index.name == 'body'
         assert confusion_df.index.name == 'ground_truth'
-        feather.write_feather(tbar_df.reset_index(), f'{path}/nt-tbar.feather')
-        feather.write_feather(body_df.reset_index(), f'{path}/nt-body.feather')
+        cache_dataframe(tbar_df.reset_index(), f'{path}/nt-tbar.feather')
+        cache_dataframe(body_df.reset_index(), f'{path}/nt-body.feather')
         if confusion_df is not None:
-            feather.write_feather(confusion_df.reset_index(), f'{path}/nt-confusion.feather')
+            cache_dataframe(confusion_df.reset_index(), f'{path}/nt-confusion.feather')
 
     def load_from_file(self, path):
         tbar_df = feather.read_feather(f'{path}/nt-tbar.feather').set_index('point_id')
@@ -344,6 +344,7 @@ def _compute_body_neurotransmitters(cfg, tbar_df, ann):
     _set_body_exp_gt_based_columns(cfg, body_df)
     body_df = body_df.drop(columns=['cell_type'], errors='ignore')
 
+    replace_object_nan_with_none(body_df)
     return body_df, confusion_df
 
 

flyem_snapshot/inputs/synapses.py
@@ -3,19 +3,16 @@
 Import synapses from disk, filter them, and associate each point with a body ID.
 """
 import os
-import re
 import copy
 import glob
-import json
 import logging
 
 import numpy as np
 import pyarrow.feather as feather
 
-from neuclease import PrefixFilter
 from neuclease.util import Timer, encode_coords_to_uint64, decode_coords_from_uint64
 
-from ..util import checksum
+from ..util import checksum, cache_dataframe
 from ..caches import cached, SerializerBase
 
 logger = logging.getLogger(__name__)
@@ -131,11 +128,11 @@ class SynapseSerializerBase(SerializerBase):
         # since that stuff is already in the directory name.
         # But it's convenient if we want to copy these files for other people to use.
         name = os.path.split(path)[-1]
-        feather.write_feather(
+        cache_dataframe(
             point_df.reset_index(),
             f'{path}/point_df-{name}.feather'
         )
-        feather.write_feather(
+        cache_dataframe(
             partner_df,
             f'{path}/partner_df-{name}.feather'
         )

commit 935870c6b7f15f197341f2431a9b9cbdd489b0e0

Fix arguments to replace() when replacing NaN with None. Bug was introduced in a51287d015393ad4e58d2c4b265fdc991459f63b

flyem_snapshot/util.py
@@ -60,7 +60,7 @@ def replace_object_nan_with_none(df):
     for col, dtype in df.dtypes.items():
         if dtype != object:
             continue
-        df[col].replace([np.nan, None], inplace=True)
+        df[col].replace([np.nan], [None], inplace=True)
 
 
 def dataframe_checksum(df):