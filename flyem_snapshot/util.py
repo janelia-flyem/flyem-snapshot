f147154b9d20f43ebd8e09d2769d58de06a2d7b1
Stuart Berg <bergs@janelia.hhmi.org>
2024-08-20 21:53:06 -0400

    Use CityHash for checksum instead of adler32

--- flyem_snapshot/util.py
+++ flyem_snapshot/util.py
@@ -2,7 +2,6 @@ import os
 import time
 import platform
 
-from zlib import adler32
 from pathlib import Path
 from datetime import datetime
 from subprocess import check_output
@@ -11,6 +10,7 @@ from collections.abc import Mapping, Sequence
 import numpy as np
 import pandas as pd
 import pyarrow.feather as feather
+from cityhash import CityHash64
 from bokeh.plotting import output_file, save as bokeh_save
 from bokeh.io import export_png
 
@@ -69,45 +69,45 @@ def dataframe_checksum(df):
     checksums.append(series_checksum(df.index))
     for c in df.columns:
         checksums.append(series_checksum(df[c]))
-    return adler32(np.array(checksums))
+    return CityHash64(np.array(checksums))
 
 
 def series_checksum(s):
     if s.dtype == 'category':
-        return adler32(s.cat.codes.values)
+        return CityHash64(s.cat.codes.values)
     if s.dtype == 'object':
-        return adler32(s.astype(str).values.astype(str))
+        return CityHash64(s.astype(str).values.astype(str))
     else:
-        return adler32(s.values)
+        return CityHash64(s.values)
 
 
 def checksum(data):
     if data is None:
         return 999999999
     if isinstance(data, str):
-        return adler32(data.encode('utf-8'))
+        return CityHash64(data.encode('utf-8'))
     if isinstance(data, (int, float)) or np.issubdtype(type(data), np.number):
-        return adler32(str(data).encode('utf-8'))
+        return CityHash64(str(data).encode('utf-8'))
     if isinstance(data, np.ndarray):
-        return adler32(data)
+        return CityHash64(data)
     if isinstance(data, pd.DataFrame):
         return dataframe_checksum(data)
     if isinstance(data, pd.Series):
         return dataframe_checksum(data.to_frame())
     if isinstance(data, Sequence):
         if all(isinstance(d, (int, float)) for d in data):
-            return adler32(np.array(data))
-        return adler32(np.array([checksum(e) for e in data]))
+            return CityHash64(np.array(data))
+        return CityHash64(np.array([checksum(e) for e in data]))
     if isinstance(data, Mapping):
         csums = []
         for k,v in sorted(data.items()):
             csums.append(checksum(k))
             csums.append(checksum(v))
-        return adler32(np.array(csums))
+        return CityHash64(np.array(csums))
 
     # Doesn't match any supported types, but we can see if the object
     # can be checksummed directly (i.e. supports buffer protocol)
-    return adler32(data)
+    return CityHash64(data)
 
 
 def export_bokeh(p, filename, title, directory=""):
