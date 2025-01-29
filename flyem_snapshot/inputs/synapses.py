aa34aaa5bef07b1485deae89483aabbf82cbc211
Stuart Berg <bergs@janelia.hhmi.org>
2025-01-29 01:09:02 -0500

    synapses: Allow point_id to be in the index or the columns (newer feather files might have either)

--- flyem_snapshot/inputs/synapses.py
+++ flyem_snapshot/inputs/synapses.py
@@ -177,6 +177,11 @@ def _load_raw_synapses(cfg):
         point_df = feather.read_feather(points_path)
         partner_df = feather.read_feather(partners_path)
 
+    # Temporarily ensure point_id is in the columns to simplify the logic below.
+    # (We'll move it to the index below.)
+    if 'point_id' not in point_df.columns and point_df.index.name == 'point_id':
+        point_df = point_df.reset_index()
+
     if 'point_id' not in point_df.columns and not {*'zyx'} <= {*point_df.columns}:
         raise RuntimeError("Synapse point table doesn't have coordinates or point_id")
 
