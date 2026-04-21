61e2ddc9d05a4baa57835c6005cb292f313a7d6c
Stuart Berg <bergs@janelia.hhmi.org>
2026-04-20 23:07:24 -0400

    elements: Optionally load from DVID instead of a file

--- flyem_snapshot/inputs/elements.py
+++ flyem_snapshot/inputs/elements.py
@@ -1,7 +1,7 @@
 """
 Import "elements" (point objects) from disk, filter them, and associate each point with a body ID.
 """
-import os
+import copy
 import logging
 
 import numpy as np
@@ -9,7 +9,12 @@ import pandas as pd
 import pyarrow.feather as feather
 
 from neuclease.util import Timer, encode_coords_to_uint64
+from neuclease.dvid import fetch_all_elements, fetch_labels_batched
 
+from .annotations import PointAnnotationSchema
+
+ElementPointAnnotationSchema = copy.deepcopy(PointAnnotationSchema)
+ElementPointAnnotationSchema['properties']['instance']['default'] = ""
 
 logger = logging.getLogger(__name__)
 
@@ -20,16 +25,29 @@ ElementTableSchema = {
     "properties": {
         "point-table": {
             "description":
-                "Optional. A feather or CSV file containing the element points, optionally with a 'body' column.\n"
+                "A feather or CSV file containing the element points, optionally with a 'body' column.\n"
                 "If an 'sv' column is also present, it can be used to much more efficiently update the body column if needed.\n"
-                "Required columns are x,y,z,type.  Other columns may be present and will be loaded in neuprint outputs.\n",
+                "Required columns are x,y,z,type.  Other columns may be present and will be loaded in neuprint outputs.\n"
+                "(If your annotation points are stored in a DVID point annotation instance, use the point-annotations config instead of this.)\n",
             "type": "string",
             "default": ""
+        },
+        "dvid-point-annotations": {
+            **ElementPointAnnotationSchema,
+            "description":
+                "Point annotation dataset from DVID to use for populating properties on the :Element nodes.\n"
+                "(If your annotation points are stored in a CSV or feather file, use the point-table config instead of this.)\n"
+                "Should look something like this:\n"
+                "dvid-point-annotations:\n"
+                "  instance: nuclei-centroids\n"
+                "  column-name: soma_position\n"
+                "  extract-properties:\n"
+                "    radius: nucleus_radius\n",
         },
         "type": {
             "description":
                 "Optional. Adds a column named 'type' to the table, with the given value in all rows.\n"
-                "If no type is provided here in the config, then the data itself should have a type column.\n",
+                "If no type is provided here in the config, then the data itself should have a type column/property.\n",
             "type": "string",
             "default": "",
         },
@@ -54,7 +72,7 @@ ElementTableSchema = {
                     "type": "null"
                 }
             ]
-        },
+        }
     }
 }
 
@@ -81,7 +99,7 @@ def load_elements(cfg, pointlabeler):
         if name == "processes":
             continue
 
-        point_df = _load_element_points(name, table_cfg)
+        point_df = _load_element_points(name, table_cfg, pointlabeler)
         distance_df = _load_element_distances(name, table_cfg)
 
         if point_df is not None:
@@ -96,17 +114,21 @@ def load_elements(cfg, pointlabeler):
     return element_dfs
 
 
-def _load_element_points(name, table_cfg):
-    path = table_cfg['point-table']
-    if not path:
+def _load_element_points(name, table_cfg, pointlabeler):
+    table_path = table_cfg['point-table']
+    ann_instance = table_cfg['dvid-point-annotations']['instance']
+
+    if ann_instance and table_path:
+        raise RuntimeError(f"Element table '{name}' cannot use both a point-annotation instance and a point-table file.")
+
+    if not table_path and not ann_instance:
         return
-    os.makedirs('tables', exist_ok=True)
-    with Timer(f"Loading '{name}' elements from disk", logger):
-        assert path.split('.')[-1] in ('feather', 'csv')
-        if path.endswith('.csv'):
-            element_df = pd.read_csv(path)
-        else:
-            element_df = feather.read_feather(path)
+    
+    if table_path:
+        element_df = _load_element_points_from_table(name, table_path)
+    if ann_instance:
+        pa_cfg = table_cfg['dvid-point-annotations']
+        element_df = _load_element_points_from_dvid(name, pa_cfg, pointlabeler)
 
     if (cfg_type := table_cfg['type']):
         if 'type' in element_df.columns and (element_df['type'] != cfg_type).any():
@@ -130,12 +152,52 @@ def _load_element_points(name, table_cfg):
     return element_df
 
 
+def _load_element_points_from_table(name, table_path):
+    with Timer(f"Loading '{name}' elements from disk", logger):
+        assert table_path.split('.')[-1] in ('feather', 'csv')
+        if table_path.endswith('.csv'):
+            element_df = pd.read_csv(table_path)
+        else:
+            element_df = feather.read_feather(table_path)
+    return element_df
+
+
+def _load_element_points_from_dvid(name, pa_cfg, pointlabeler):
+    with Timer(f"Loading '{name}' elements from DVID instance '{pa_cfg['instance']}'", logger):
+        df = fetch_all_elements(
+            pointlabeler.dvidseg.server,
+            pointlabeler.dvidseg.uuid,
+            pa_cfg['instance'],
+            format='pandas'
+        )
+    df = df.sort_values([*'zyx'])
+
+    # Assign a column for each of the extracted point properties.
+    for prop, propcol in pa_cfg['extract-properties'].items():
+        if prop.lower() not in df:
+            logger.warning(f"Annotation instance {pa_cfg['instance']} contains no properties named '{prop}'")
+            continue
+
+        # DVID annotation properties are stored as strings,
+        # even if they would ideally be stored as int or float.
+        # Attempt to convert back to float if possible,
+        # otherwise leave as string.
+        try:
+            df[prop.lower()] = df[prop.lower()].astype(np.float32)
+        except ValueError:
+            logger.warning(f"Annotation instance {pa_cfg['instance']} contains non-numeric property '{prop}'; keeping as string.")
+
+        df[propcol] = df[prop.lower()]
+
+    df = df[[*'zyx', *pa_cfg['extract-properties'].values()]]
+    return df
+
+
 def _load_element_distances(name, table_cfg):
     path = table_cfg['distance-table']
     if not path:
         return
 
-    os.makedirs('tables', exist_ok=True)
     with Timer(f"Loading '{name}' distances from disk", logger):
         distance_df = feather.read_feather(path)
         required_cols = {
